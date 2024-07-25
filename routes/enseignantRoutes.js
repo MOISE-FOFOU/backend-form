8bca43bb45934163d929d9cfff849f4905d47122
FOFOU KUETE MOISE <129035192+MOISE-FOFOU@users.noreply.github.com>
2024-07-25 20:39:47 +0200

    Update enseignantRoutes.js

--- routes/enseignantRoutes.js
+++ routes/enseignantRoutes.js
@@ -6,9 +6,9 @@ const authenticateToken = require('../controllers/authenticateToken');
 
 
 router.get('/Getenseignant/:id?',Enseignant.Getenseignant)
-router.get('/Getinfosenseignant/:id',Enseignant.Getinfosenseignant)
+router.get('/Getinfosenseignant/:id1/:id2',Enseignant.Getinfosenseignant)
 router.post('/Postenseignant',authenticateToken, Enseignant.Postenseignant);
-router.post('/Updateinfosenseignants',Enseignant.Updateinfosenseignants);
+router.post('/Updateinfosenseignants/:id1/:id2',Enseignant.Updateinfosenseignants);
 router.post('/Updateenseignant/:id',authenticateToken,Enseignant.Updateenseignant)
 router.post('/ActivateEnseignant/:id',authenticateToken,Enseignant.ActivateEnseignant)
 router.get('/Enseignant',authenticateToken, Enseignant.Enseignant);
