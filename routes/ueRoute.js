3be4b124ba725a173d105d2f40eac0295d3a39bc
FOFOU KUETE MOISE <129035192+MOISE-FOFOU@users.noreply.github.com>
2024-07-28 09:40:53 -0400

    Update ueRoute.js

--- routes/ueRoute.js
+++ routes/ueRoute.js
@@ -11,7 +11,7 @@ router.get('/Getueinfos/:id1/id2',Ue.Getueinfos)
 
 router.post('/Postue',authenticateToken, Ue.Postue);
 router.post('/Updateue/:id', authenticateToken, Ue.Updateue);
-router.post('/Activateue/:id',authenticateToken,Actualites.Activateue)
+router.post('/Activateue/:id',authenticateToken,Ue.Activateue)
 router.get('/Ue',authenticateToken, Ue.Ue);
 
 module.exports = router;
