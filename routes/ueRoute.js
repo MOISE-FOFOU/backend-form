3ad4e887b0be8d7e6b7c7c4b4be2b5876336d392
FOFOU KUETE MOISE <129035192+MOISE-FOFOU@users.noreply.github.com>
2024-07-28 09:00:13 -0400

    Update ueRoute.js

--- routes/ueRoute.js
+++ routes/ueRoute.js
@@ -1,1 +1,17 @@
 
+const express=require('express')
+const Ue=require('../controllers/Controller')
+const router=express.Router()
+const authenticateToken = require('../controllers/authenticateToken');
+
+
+
+router.get('/Getue/:id?',Ue.Getue)
+router.get('/Getue/:id1/id2',Ue.Getue)
+
+router.post('/Postue',authenticateToken, Ue.Postue);
+router.post('/Updateue/:id', authenticateToken, Ue.Updateue);
+router.post('/Activateue/:id',authenticateToken,Actualites.Activateue)
+router.get('/Ue',authenticateToken, Ue.Ue);
+
+module.exports = router;
